e6d319f08f2450e5e63792ebaf410954b249c849
JairoCoolBeans <jairomata1230@gmail.com>
2026-04-18 18:40:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+  const darkBtn = document.getElementById("darkModeBtn");
+  const factBtn = document.getElementById("funFactBtn");
+  const factText = document.getElementById("funFact");
+
+  // Dark mode toggle
+  darkBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+  });
+
+  //Fun fact interaction
+  factBtn.addEventListener("click", () => {
+    factText.textContent = "Fun Fact: I enjoy catching lizards when I see them!";
+  });
+
+});
